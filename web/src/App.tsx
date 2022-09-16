7b8762610e52af54ce7c805d98e6aefe351465f4
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2022-09-15 22:07:13 -0300

    ambiente react pronto

---
+++ web/src/App.tsx
@@ -0,0 +1,18 @@
+
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <button>
+      {props.title}
+    </button>
+  )
+}
+
+function App() {
+  return <h1>Hello World</h1>
+}
+
+export default App
