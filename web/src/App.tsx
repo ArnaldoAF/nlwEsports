fad5208d48a168685f9f7dc996443c379fbde007
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2022-10-03 12:36:34 -0300

    titulo e imagens dos jogos

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,22 +1,38 @@
-import './styles/main.css';
-interface ButtonProps {
-  title: string;
-}
+import "./styles/main.css";
 
-function Button(props: ButtonProps) {
-  return (
-    <button>
-      {props.title}
-    </button>
-  )
-}
+import logoImg from "./assets/logo-nlw-esports.svg";
 
 function App() {
   return (
-    <div className="w-8 h-8 bg-black light:bg-violet-500">
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logoImg} alt="" />
 
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu <span className="bg-nlw-gradient bg-clip-text text-transparent"> duo </span> está aqui
+      </h1>
+
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        <a href="">
+          <img src="/game-1.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game-2.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game-3.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game-4.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game-5.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game-6.png" alt="" />
+        </a>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
