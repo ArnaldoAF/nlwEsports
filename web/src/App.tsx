7c72a4fbe3a4e0336525f197ff214b73c5f595d4
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2022-10-03 13:00:06 -0300

    estilização imagens de jogos

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -12,23 +12,53 @@ function App() {
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <a href="">
+        <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/game-1.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute right-0 left-0 bottom-0">
+            <strong className="text-white font-bold block">Título</strong>
+            <span className="text-zinc-300 text-sm block">0 anúncios</span>
+          </div>
         </a>
-        <a href="">
+        <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/game-2.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute right-0 left-0 bottom-0">
+            <strong className="text-white font-bold block">Título</strong>
+            <span className="text-zinc-300 text-sm block">0 anúncios</span>
+          </div>
         </a>
-        <a href="">
+        <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/game-3.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute right-0 left-0 bottom-0">
+            <strong className="text-white font-bold block">Título</strong>
+            <span className="text-zinc-300 text-sm block">0 anúncios</span>
+          </div>
         </a>
-        <a href="">
+        <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/game-4.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute right-0 left-0 bottom-0">
+            <strong className="text-white font-bold block">Título</strong>
+            <span className="text-zinc-300 text-sm block">0 anúncios</span>
+          </div>
         </a>
-        <a href="">
+        <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/game-5.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute right-0 left-0 bottom-0">
+            <strong className="text-white font-bold block">Título</strong>
+            <span className="text-zinc-300 text-sm block">0 anúncios</span>
+          </div>
         </a>
-        <a href="">
+        <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/game-6.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute right-0 left-0 bottom-0">
+            <strong className="text-white font-bold block">Título</strong>
+            <span className="text-zinc-300 text-sm block">0 anúncios</span>
+          </div>
         </a>
       </div>
     </div>
