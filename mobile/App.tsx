0a25745e2883aeefefcfaba5e27b90d8140bc162
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2022-10-03 15:55:43 -0300

    mobile: spash screen feita

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,34 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import { Background } from "./src/components/Background";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Button title="teste" />
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  <Background>
 
-interface ButtonProps {
-  title:string;
+  </Background>);
 }
-
-function Button(props: ButtonProps){
-  return (
-    <TouchableOpacity> 
-      <Text>{props.title}</Text>
-    </TouchableOpacity>
-  )
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
