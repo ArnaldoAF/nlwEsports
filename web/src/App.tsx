703187f23a62fc4ebfbbb109c9a2e043df6546a2
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2022-10-03 08:47:08 -0300

    assets e instalando tailwind

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,4 @@
-
+import './styles/main.css';
 interface ButtonProps {
   title: string;
 }
@@ -12,7 +12,11 @@ function Button(props: ButtonProps) {
 }
 
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <div className="w-8 h-8 bg-black light:bg-violet-500">
+
+    </div>
+  )
 }
 
 export default App
