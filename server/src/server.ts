73be7358e24805a305cf2c9a455bc6d0509a8140
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2022-10-04 16:33:48 -0300

    server: list games and ads

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,50 @@
 import express, { response } from 'express';
+import { PrismaClient} from '@prisma/client';
 
 const app = express();
+const prisma = new PrismaClient({
+    log: ['query']
+});
 
-app.get('/games', (request, response) => { 
-    return response.json([])
+app.get('/games', async (request, response) => { 
+    
+    const games = await prisma.game.findMany({
+        include: {
+            _count: {
+                select: {
+                    Ad: true
+                }
+            }
+        }
+    });
+    return response.json(games)
 })
 
-app.get('/games/:id/ads', (request, response) => { 
-    return response.json([])
+app.get('/games/:id/ads', async (request, response) => { 
+    const gameId = request.params.id;
+    const ads = await prisma.ad.findMany({
+        select: {
+            id: true,
+            name: true,
+            weekDays: true,
+            useVoiceChannel: true,
+            yearsPlaying: true,
+            hourStart: true,
+            hourEnd: true,
+        },
+        where: {
+            gameId,
+        },
+        orderBy: {
+            creatAt: 'desc'
+        }
+    })
+    return response.json(ads.map(ad => {
+        return { 
+            ...ad,
+            weekDays: ad.weekDays.split(',')
+        }
+    }))
 })
 
 app.post('/ads', (request, response) => { 
