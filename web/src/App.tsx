4d6f74f6250bdb4a6a8b5813b3a1a883313d6d1b
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2022-10-05 12:06:49 -0300

    web: list games

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { MagnifyingGlassPlus } from "phosphor-react";
 
 import "./styles/main.css";
@@ -6,7 +7,27 @@ import logoImg from "./assets/logo-nlw-esports.svg";
 import { GameBanner } from "./components/GameBanner";
 import { CreateAdBanner } from "./components/CreateAdBanner";
 
+interface Game {
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number;
+  };
+}
+
 function App() {
+  const [games, setGames] = useState<Game[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3333/games")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setGames(data);
+      });
+  }, []);
+
   return (
     <div className="max-w-[1200px] mx-auto flex flex-col items-center my-20">
       <img src={logoImg} alt="" />
@@ -20,12 +41,16 @@ function App() {
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <GameBanner
-          bannerUrl="/game-1.png"
-          title="League of Legends"
-          adsCount={2}
-        />
-        
+        {games.map((game) => {
+          return (
+            <GameBanner
+              bannerUrl={game.bannerUrl}
+              title={game.title}
+              adsCount={game._count.ads}
+              key={game.id}
+            />
+          );
+        })}
       </div>
 
       <CreateAdBanner />
