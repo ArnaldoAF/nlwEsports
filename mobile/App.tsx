70dd612d2e239c2d858d3a6f791d598db094b286
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2022-10-06 11:17:24 -0300

    mobile: navigation to game screen

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,15 +1,15 @@
-import { StatusBar } from 'react-native';
+import { StatusBar } from "react-native";
 import {
   useFonts,
   Inter_400Regular,
   Inter_600SemiBold,
   Inter_700Bold,
   Inter_900Black,
-} from '@expo-google-fonts/inter';
+} from "@expo-google-fonts/inter";
 
 import { Background } from "./src/components/Background";
-import {Home} from './src/screens/Home';
-import { Loading } from './src/components/Loading';
+import { Routes } from "./src/routes";
+import { Loading } from "./src/components/Loading";
 
 export default function App() {
   const [fontsLoad] = useFonts({
@@ -19,13 +19,13 @@ export default function App() {
     Inter_900Black,
   });
   return (
-  <Background>
-    <StatusBar 
-      barStyle="light-content"
-      backgroundColor="transparent"
-      translucent
-    />
-    {fontsLoad ? <Home /> : <Loading />}
-
-  </Background>);
+    <Background>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoad ? <Routes /> : <Loading />}
+    </Background>
+  );
 }
