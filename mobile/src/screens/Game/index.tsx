c889820f1a90d326587010809e80989092384205
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2022-10-06 11:21:54 -0300

    mobile: passando parametros pra game

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useRoute } from "@react-navigation/native";
 import { Background } from "../../components/Background";
 
 import { styles } from "./styles";
+import { GameParams } from "../../@types/navigation";
+
+interface RouteParams {
+  id: string;
+  title: string;
+  bannerUrl: string;
+}
 
 export function Game() {
+  const route = useRoute();
+  const game = route.params as GameParams;
   return (
     <Background>
       <SafeAreaView style={styles.container}></SafeAreaView>
